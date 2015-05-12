32f4e31ce7dfc4ed75160252e58cf99155a4c1a6
Leelu55 <maria@yalpani.com>
2015-05-12 21:54:00 +0200

    cleaned up the mess with proper use of underscorejs

--- cleanAddresses.js
+++ cleanAddresses.js
@@ -1,42 +1,27 @@
 var stdin = require('get-stdin')
 var _ = require('underscore')
-var addresses = []
-
+var rawAddressArray = []
+var cleanAddressArray = []
 
 stdin(function(data) {
   data = JSON.parse(data)
-  //console.log(data.length)
-  var rawAddressArray = []
   
   _.each(data, function(item){
-    //console.log(item.id + "\n")
-    
-    for (var i = 0; i < item.authors.length; i++) {
-      //console.log(i + " " + JSON.stringify(item.authors[i].author) + "\n")
-        for (var j = 0; j < (item.authors[i].addresses).length; j++) {
-          rawAddressArray.push((item.authors[i].addresses[j]).split(","))
-        }
+    item.authors.map(function(author) {
       
-    }
-    return rawAddressArray
-    //console.log(item)
+      for (i = 0; i < author.addresses.length; i++)
+        rawAddressArray.push(author.addresses[i])
+    })
   })
-  //console.log(addressArray.length)
-  
-  var addressArray = []
-  
   
   for (i = 0; i < rawAddressArray.length; i++) {
-    cleanAddress = ((((rawAddressArray[i]).toString()).trim()).split(".")[0])
-    
-    var record = {
-      address : cleanAddress, 
-      latLng  : ''
+    var add = {
+      address : rawAddressArray[i].trim()
     }
-    addresses.push(record)
+    cleanAddressArray.push(add)
   }
   
-  for (i = 0; i < addresses.length; i++) 
-    console.log(addresses[i])
+  for (i = 0; i < cleanAddressArray.length; i++)
+    console.log(cleanAddressArray[i])
 })
 
